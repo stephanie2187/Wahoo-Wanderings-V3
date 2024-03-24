10d5e70f0a6dbafce32d039db15df7b5718b2427
Stephanie Lim <118370387+stephanie2187@users.noreply.github.com>
2024-03-24 07:52:33 -0400

    Add files via upload

---
+++ signUp/signUp.js
@@ -0,0 +1,40 @@
+const usernameInput = document.querySelector("#username")
+const passwordInput = document.querySelector("#password")
+
+document.addEventListener('keypress',function(event){
+    if (event.key=="Enter"){
+        const username = usernameInput.value
+        const password = passwordInput.value
+
+        const url = `https://wahoowanderings.co/user/addUser/${username}/${password}`
+
+        var newTab = window.open(url,'_blank')
+
+        let validity;
+        let placesVisited;
+        var interval = setInterval(function(){
+            const xhr = new XMLHttpRequest()
+            xhr.open('GET',url,true)
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status===200){
+                        const response = JSON.parse(xhr.responseText);
+                        validity = response.value;
+                        placesVisited = response.strings
+
+
+                        clearInterval(interval)
+                        newTab.close();
+                    }
+                }
+            }
+            xhr.send()
+        },200)
+
+        if (validity===true){
+            localStorage.setItem('placesVisited',JSON.stringify(placesVisited))
+            localStorage.setItem('isLoggedIn',JSON.stringify(true))
+            window.location.href = 'https://wahoowanderings.co'
+        }
+    }
+})
